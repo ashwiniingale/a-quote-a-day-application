558e0b0a93f2972839209feb0e401c848997675b
Ashwini Ingale <Ashwini.Ingale@wu.com>
2018-11-19 20:48:29 +0530

    adding all files

---
+++ index.js
@@ -0,0 +1,13 @@
+var express = require('express');
+var app = express();
+
+
+app.get("/", function(request, response){
+    response.sendFile(__dirname + "/index.html")
+});
+
+var PORT = process.env.PORT || 3000;
+
+app.listen(PORT,  function(request, response){
+    console.log("Listening On port :" + PORT);
+});
